ea50d20caf3097177401b4a5792b1ec41ece2f74
mygithub-soumya <77545127+mygithub-soumya@users.noreply.github.com>
2021-08-23 14:16:08 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,5 +3,6 @@ public static void main(String args[]) throws InterruptedException{
 Thread.sleep(10000);
 System.out.println("Hellow Java updated");
   System.out.println("Hellow Java updated 2");
+  System.out.println("Hello Java updated 3");
 }
 }
